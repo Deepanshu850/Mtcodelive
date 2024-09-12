f566f60fb7d218ea3991fe8b242e885fe15ac8fc
Chetan <chetan25december@gmail.com>
2024-09-12 12:05:12 +0530

    getting tempalte files

---
+++ template.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0, minimum-scale=1.0">
+
+    <title>
+        
+    </title>
+
+    <meta name="description" content="" />
+
+    <meta name="keywords" content="" />
+
+    <meta name="Owner" content="Moneytreerealty.com" />
+    <meta name="Copyright" content="Moneytreereal.com" />
+    <meta name="classification" content="About Real Estate Consulting" />
+    <meta name="distribution" content="India" />
+    <meta name="rating" content="General" />
+    <meta name="subject" content="Learn about MoneyTree Realty's mission, services, and how we can assist you in navigating the real estate market in India" />
+    <meta name="audience" content="All" />
+
+
+    <meta property="og:title" content="Kukatpally Housing Board Colony, Hyderabad">
+    <meta property="og:site_name" content="Moneytree Realty">
+    <meta property="og:url" content="https://moneytreerealty.com/">
+    <meta property="og:description" content="Kukatpally Housing Board Colony in Hyderabad is a residential area known for its excellent connectivity, amenities, property rates, and top builders' commercial hubs.">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://moneytreerealty.com/assets/img/">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:image:alt" content="Kukatpally Housing Board Colony in Hyderabad">
+    <meta property="og:locale" content="en_IN">
+  
+
+    <link rel="canonical" href="https://moneytreerealty.com/" />
+
+
+
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-N2ZBWP94');
+    </script>
+    <!-- End Google Tag Manager -->
+
+
+
+
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="./assets/css/nice-select.css" rel="stylesheet">
+    <link rel="icon" href="./assets/img/favicon_io/favicon.ico" type="image/x-icon" sizes="20x20">
+    <link rel="stylesheet" href="./assets/css/property-page.css">
+
+
+
+    <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JZJY23MWW7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-JZJY23MWW7');
+    </script>
+    <!-- End Google Analytics -->
+    <!--G-JZJY23MWW7-->
+</head>
+
+<body class="light-theme">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2ZBWP94" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+
+
+
+    <?php include './header.php';  ?>
+
+
+    <!-- main content -->
+    <div class="side-flex">
+
+        <main>
+          <div class="image-wrapper">
+              <img src="./assets/img/ " 
+              alt="">
+          </div>
+  
+          <h1>
+              
+          </h1>
+
+
+
+
+      </main>
+
+        <div class="side-flex-right">
+            <?php
+            include './recent-page-component.php';
+            ?>
+
+            <div class="single-widgets widget_egns_tag">
+                <div class="widget-title mb-20">
+                    <h6 style="color:#005b52;text-align: center;font-size:18px;">Popular Tags</h6>
+                </div>
+
+
+                <p class="wp-block-tag-cloud">
+
+               
+
+
+                </p>
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <div class="blog-tag-social-area">
+        <div class="tag-wrapper">
+            <h6>Similar Properties:</h6>
+            <ul class="tags">
+               
+            
+            
+
+
+                <li>
+                    <a href="./index.php">
+                        Residential property in Noida
+
+                    </a>
+                </li>
+                <li>
+                    <a href="./index.php">
+                        Noida apartments for sale
+
+                    </a>
+                </li>
+                <li>
+                    <a href="./studio-apartments-in-noida.php">
+                        studio apartment
+
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="./index.php">
+                        best property in noida
+
+                    </a>
+                </li>
+                <li>
+                    <a href="./index.php">
+                        Buy property in Noida
+
+                    </a>
+                </li>
+                <li>
+                    <a href="./index.php">
+                        best commercial property in noida
+
+                    </a>
+                </li>
+                <li>
+                    <a href="./studio-apartments-in-noida.php">
+                        Noida Real Estate
+
+                    </a>
+                </li>
+                <li>
+                    <a href="./index.php">
+                        Real estate builders in Noida
+
+                    </a>
+                </li>
+
+
+
+            </ul>
+        </div>
+
+
+        <?php
+        include './more-tags.php';
+        include './footer.php';
+        ?>
+
+
+    </div>
+
+    <script src="./assets/js/swiper-bundle.min.js"></script>
+    <script src="./assets/js/slick.js"></script>
+    <script src="./assets/js/jquery.counterup.min.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.fancybox.min.js"></script>
+    <script src="./assets/js/custom.js"></script>
+
+
+
+</body>
+
+</html>
